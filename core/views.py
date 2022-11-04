68a905dd5d25db6689f0b64f0e49081f53d1f34f
Bruno Castro <bs8872491@gmail.com>
2022-11-04 12:04:51 -0300

    Adicionando as templates de login e cadastro

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'core/index.html')
+
+def login(request):
+    return render(request, 'core/login.html')
+
+def register(request):
+    return render(request, 'core/register.html')
     
