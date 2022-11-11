d73b2f90d9579740f91a1c99bea370e1402546f1
Bruno Castro <bs8872491@gmail.com>
2022-11-11 10:09:58 -0300

    Desenvolvendo o logout e validando a autenticação do usuário

--- user/urls.py
+++ user/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
+    path('logout', views.logout, name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib import messages
+from core import views
 
 def dashboard(request):
-    return render(request, 'user/dashboard.html')
+    if request.user.is_authenticated:
+        return render(request, 'user/dashboard.html')
+    else:
+        return redirect(views.home)
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'Logout realizado com sucesso')
+    return redirect(views.home)
     
